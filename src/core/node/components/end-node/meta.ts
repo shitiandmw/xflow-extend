4b471e0d1e8ec53f71d39e5d8d4c4faa868040c7
shitiandmw <lushitian405@gmail.com>
2024-07-10 11:28:19 +0800

    增加开始和结束节点的属性

--- src/core/node/components/end-node/meta.ts
+++ src/core/node/components/end-node/meta.ts
@@ -1,4 +1,26 @@
 export default {
     title: "结束节点",
-    category: "节点"
+    category: "节点",
+    props: [
+        {
+            name: "label",
+            title: "标题",
+            propType: "string",
+            setter: "StringSetter",
+            defaultValue: "结束节点"
+        },
+        {
+            name: "desc",
+            title: "说明",
+            propType: "object",
+            setter: {
+                name: "DescSetter",
+                props: {
+                    editable: false,
+                    content: "标志流程结束的节点，通常作为最后一个节点出现。"
+                }
+            }
+        },
+
+    ]
 }
--- src/core/node/components/start-node/meta.ts
+++ src/core/node/components/start-node/meta.ts
@@ -1,4 +1,26 @@
 export default {
     title: "开始节点",
-    category: "节点"
+    category: "节点",
+    props: [
+        {
+            name: "label",
+            title: "标题",
+            propType: "string",
+            setter: "StringSetter",
+            defaultValue: "开始节点"
+        },
+        {
+            name: "desc",
+            title: "说明",
+            propType: "object",
+            setter: {
+                name: "DescSetter",
+                props: {
+                    editable: false,
+                    content: "标志流程开始的节点，通常作为第一个节点出现。"
+                }
+            }
+        },
+
+    ]
 }
